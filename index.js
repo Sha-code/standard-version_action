19e55ca8b7282f1f2c00370ae0f6130de87ec949
Sha_code <shannone.controu@gmail.com>
2020-10-07 00:10:52 +0200

    feat: push v0 TODO need test

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core");
+const standardVersion = require("standard-version");
+const { exec } = require("child_process");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const githubToken = core.getInput("github_token");
+    if (!githubToken) {
+      throw "Missing githubToken";
+    }
+    core.info("Create Bump");
+    const options = {};
+    const prerelease = core.getInput("prerelease");
+    if (prerelease) {
+      options["prerelease"] = prerelease;
+    }
+    core.info("Running standar version");
+    await standardVersion(options);
+    core.info("Configuring git user and email...");
+    await exec('git config --local user.email "action@github.com"');
+    await exec('git config --local user.name "GitHub Action"');
+    core.info("Pushing to branch...");
+    const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${githubToken}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+    await exec(
+      `git push "${remoteRepo}" HEAD:${process.env.GITHUB_REF} --follow-tags --tags`
+    );
+    const pjson = require("./package.json");
+    core.info(`Released version ${pjson.version}`);
+    core.setOutput("version", pjson.version);
   } catch (error) {
     core.setFailed(error.message);
   }
